4a0829ea55c3631429867c409159385cd9e1119e
XinlongWu <43398706+Xinlong-Wu@users.noreply.github.com>
2026-01-17 13:01:29 +0000

    Add OpenHarmony support in netmon package build constraints

--- net/netmon/interfaces_defaultrouteif_todo.go
+++ net/netmon/interfaces_defaultrouteif_todo.go
@@ -1,7 +1,7 @@
 // Copyright (c) Tailscale Inc & contributors
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build !linux && !windows && !darwin && !freebsd && !android && !openbsd
+//go:build !linux && !windows && !darwin && !freebsd && !android && !openbsd && !openharmony
 
 package netmon
 
